e42152756414cc82fa48b05dbcded2c6b9221c35
RubenLaube-Pohto <h8871@student.jamk.fi>
2016-09-08 18:23:51 +0300

    Add page for displaying basic xml data

---
+++ Saitti/GetWorkersEvening.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="GetWorkersEvening.aspx.cs" Inherits="GetWorkersEvening" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Workers of the Company</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div id="buttons">
+            <asp:Button ID="btnGet" runat="server" Text="Get workers" OnClick="btnGet_Click" />
+        </div>
+        <div id="presentation">
+            <asp:GridView ID="gvData" runat="server"></asp:GridView>
+        </div>
+        <div id="footer">
+            <asp:Label ID="lblMsg" runat="server" Text="..."></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Saitti/GetWorkersEvening.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Configuration; // for using web.config
+using System.Collections.Generic;
+using System.Data; // for ADO.NET classes
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class GetWorkersEvening : System.Web.UI.Page
+{
+    string xmlFile;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // read xml filename from web.config
+        xmlFile = ConfigurationManager.AppSettings["tiedosto"];
+        lblMsg.Text = xmlFile;
+    }
+
+    protected void btnGet_Click(object sender, EventArgs e)
+    {
+        // read workers from xml file and display them in the gridview
+
+        DataSet ds = new DataSet();
+        DataTable dt = new DataTable();
+        DataView dv = new DataView();
+
+        ds.ReadXml(Server.MapPath(xmlFile));
+        dt = ds.Tables[0];
+        dv = dt.DefaultView;
+
+        // bind data to ui control
+        gvData.DataSource = dv;
+        gvData.DataBind();
+
+        lblMsg.Text = String.Format("Found {0} workers", dt.Rows.Count);
+    }
+}
