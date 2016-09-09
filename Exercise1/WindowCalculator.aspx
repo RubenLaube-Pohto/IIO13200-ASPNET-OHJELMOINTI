c852e7dffbf9a5b165d6aa4d845439f6e0f28273
RubenLaube-Pohto <h8871@student.jamk.fi>
2016-09-09 14:00:00 +0300

    Finish exercise 1

--- Exercise1/WindowCalculator.aspx
+++ Exercise1/WindowCalculator.aspx
@@ -8,8 +8,25 @@
 </head>
 <body>
     <form id="form1" runat="server">
+    <div id="input">
+        <!-- these should be done with table in order to align properly -->
+        <asp:Label ID="lblWidth" runat="server" Text="Window width:"></asp:Label>
+        <asp:TextBox ID="txtWidth" runat="server"></asp:TextBox><br />
+        <asp:Label ID="lblHeight" runat="server" Text="Window height:"></asp:Label>
+        <asp:TextBox ID="txtHeight" runat="server"></asp:TextBox><br />
+        <asp:Label ID="lblBorderWidth" runat="server" Text="Border width:"></asp:Label>
+        <asp:TextBox ID="txtBorderWidth" runat="server"></asp:TextBox><br />
+    </div>
     <div>
-    
+        <asp:Button ID="btnCalculate" runat="server" Text="Calculate price" OnClick="btnCalculate_Click" />
+    </div>
+    <div id="output">
+        <asp:Label ID="lblArea" runat="server" Text="Window area:"></asp:Label>
+        <asp:TextBox ID="txtArea" runat="server" Enabled="false"></asp:TextBox><br />
+        <asp:Label ID="lblCirc" runat="server" Text="Border circumference:"></asp:Label>
+        <asp:TextBox ID="txtCirc" runat="server" Enabled="false"></asp:TextBox><br />
+        <asp:Label ID="lblPrice" runat="server" Text="Price (w/o VAT):"></asp:Label>
+        <asp:TextBox ID="txtPrice" runat="server" Enabled="false"></asp:TextBox><br />
     </div>
     </form>
 </body>
--- Exercise1/WindowCalculator.aspx.cs
+++ Exercise1/WindowCalculator.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -10,5 +11,45 @@ public partial class WindowCalculator : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void btnCalculate_Click(object sender, EventArgs e)
+    {
+        // cm -> m
+        float w = float.Parse(txtWidth.Text) / 100;
+        float h = float.Parse(txtHeight.Text) / 100;
+        float b = float.Parse(txtBorderWidth.Text) / 100;
+
+        txtArea.Text = CalcArea(w, h).ToString();
+        txtCirc.Text = CalcBorderCirc(w, h, b).ToString();
+        txtPrice.Text = CalcPrice(w, h, b).ToString();
+    }
+
+    private float CalcArea(float w, float h)
+    {
+        return w * h;
+    }
+
+    private float CalcBorderCirc(float w, float h, float b)
+    {
+        //return 2 * (w + 2 * b) + 2 * (h + 2 * b);
+        //return 2 * w + 4 * b + 2 * h + 4 * b;
+        //return 2 * w + 2 * h + 8 * b;
+        return 2 * (w + h + 4 * b);
+    }
+
+    private float CalcPrice(float w, float h, float b)
+    {
+        //(1 + kate%) x ((IkkunanPintaAla x LasinNeliohinta) + (KarminPiiri x AlumiiniKarminJuoksumetriHinta) + (Työmenekki))
+
+        float squarePrice = float.Parse(ConfigurationManager.AppSettings["squarePrice"]);
+        float borderPrice = float.Parse(ConfigurationManager.AppSettings["borderPrice"]);
+        float work = float.Parse(ConfigurationManager.AppSettings["work"]);
+        float profit = float.Parse(ConfigurationManager.AppSettings["profit"]);
+
+        float windowCost = CalcArea(w, h) * squarePrice;
+        float borderCost = CalcBorderCirc(w, h, b) * borderPrice;
+
+        return (1.0f + profit) * (windowCost + borderCost + work);
     }
 }
