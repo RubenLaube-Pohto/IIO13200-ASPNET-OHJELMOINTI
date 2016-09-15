41ce185668aa7298bc758d3c93010f93fc334810
RubenLaube-Pohto <h8871@student.jamk.fi>
2016-09-15 17:41:39 +0300

    Implement basic reading from database

---
+++ Saitti/GetStudentsEvening.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="GetStudentsEvening.aspx.cs" Inherits="GetStudentsEvening" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Students 16S</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1>Students Autumn 2016</h1>
+        <div id="buttons">
+            <asp:Button ID="btnGet3" runat="server" Text="Get three students" OnClick="btnGet3_Click" />
+            <asp:Button ID="btnGetAll" runat="server" Text="Get students from db" OnClick="btnGetAll_Click"/>
+        </div>
+        <div id="data">
+            <asp:GridView ID="gvStudents" runat="server"></asp:GridView>
+        </div>
+        <div id="footer">
+            <asp:Label ID="lblMessage" runat="server" Text=""></asp:Label>
+        </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Saitti/GetStudentsEvening.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data; // ado.net data classes
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+
+public partial class GetStudentsEvening : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnGet3_Click(object sender, EventArgs e)
+    {
+        // get precreated data and bind to control
+        DataTable dt = JAMK.ICT.Data.DBPlacebo.Get3TestStudents();
+        gvStudents.DataSource = dt;
+        gvStudents.DataBind();
+    }
+
+    protected void btnGetAll_Click(object sender, EventArgs e)
+    {
+        string cs = ConfigurationManager.ConnectionStrings["Students"].ConnectionString;
+        string msg = "";
+        try
+        {
+            DataTable dt = JAMK.ICT.Data.DBPlacebo.GetAllStudentsFromSQLServer(cs, "oppilaat", out msg);
+            gvStudents.DataSource = dt;
+            gvStudents.DataBind();
+            lblMessage.Text = msg;
+        }
+        catch (Exception ex)
+        {
+            // where to display the exception message?
+            lblMessage.Text = ex.Message;
+        }
+    }
+}
