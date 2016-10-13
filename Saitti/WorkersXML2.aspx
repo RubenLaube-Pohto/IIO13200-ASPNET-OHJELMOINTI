7be193956fc4db4d9807fbebc97221ce9c28e916
Ruben Laube-Pohto <h8871@student.jamk.fi>
2016-10-13 18:56:28 +0300

    Do xml manipulation exercise

---
+++ Saitti/WorkersXML2.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="WorkersXML2.aspx.cs" Inherits="WorkersXML" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+    <style>
+        img {
+            width: 100%;
+        }
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="body" Runat="Server">
+    <asp:Xml
+        ID="Xml1"
+        DocumentSource="~/App_Data/Työntekijät2013.xml"
+        TransformSource="~/App_Data/Demo2.xsl"
+        runat="server"></asp:Xml>
+    <asp:Label ID="lblMsg" runat="server" Text=""></asp:Label>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="footer" Runat="Server">
+</asp:Content>
---
+++ Saitti/WorkersXML2.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+public partial class WorkersXML : System.Web.UI.Page
+{
+    protected void Page_LoadComplete(object sender, EventArgs e)
+    {
+        XmlDocument doc = new XmlDocument();
+        doc = Xml1.Document;
+        XmlNodeList palkat = doc.SelectNodes("/tyontekijat/tyontekija[tyosuhde='vakituinen']/palkka");
+        lblMsg.Text = "Vakituisten määrä: " + palkat.Count.ToString();
+        int sum = 0;
+        foreach (XmlNode palkka in palkat)
+        {
+            sum += int.Parse(palkka.InnerText);
+        }
+        lblMsg.Text += ", Vakituisten yhteen laskettu palkka: " + sum;
+    }
+}
