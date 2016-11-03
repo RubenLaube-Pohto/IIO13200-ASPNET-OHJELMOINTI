fb2187f825af82166d809888b6bfb9e9387e4a89
Ruben Laube-Pohto <h8871@student.jamk.fi>
2016-11-03 18:42:11 +0200

    Commit lesson on using the entity framework

---
+++ Saitti/BookShop.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="BookShop.aspx.cs" Inherits="BookShop" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+    <style>
+        img {
+            width: 100%;
+        }
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="body" Runat="Server">
+    <h1>My Book Shop</h1>
+    <div class="w3-row">
+        <!-- customers -->
+        <div class="w3-container w3-third">
+            <asp:Button ID="btnGetCustomers" CssClass="w3-btn w3-blue" runat="server" Text="Get customers" OnClick="btnGetCustomers_Click" />
+            <asp:DropDownList ID="ddlCustomers" runat="server" OnSelectedIndexChanged="ddlCustomers_SelectedIndexChanged" AutoPostBack="true"></asp:DropDownList>
+            <asp:GridView ID="gvCustomers" CssClass="w3-table" runat="server"></asp:GridView>
+        </div>
+        <!-- books -->
+        <div class="w3-container w3-twothird">
+            <asp:Button ID="btnGetBooks" CssClass="w3-btn w3-blue" runat="server" Text="Get books" OnClick="btnGetBooks_Click"/>
+            <asp:GridView ID="gvBooks" CssClass="w3-table" runat="server"></asp:GridView>
+        </div>
+    </div>
+    <div class="w3-row">
+        <asp:Literal ID="ltResult" runat="server"></asp:Literal>
+    </div>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="footer" Runat="Server">
+    <asp:Label ID="lblMessage" runat="server" Text=""></asp:Label>
+</asp:Content>
+
---
+++ Saitti/BookShop.aspx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class BookShop : System.Web.UI.Page
+{
+    protected static BookShopEntities ctx;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            ctx = new BookShopEntities();
+            FillControls();
+        }
+    }
+
+    #region METHODS
+    protected void ShowCustomers()
+    {
+        gvCustomers.DataSource = ctx.Customers.ToList();
+        gvCustomers.DataBind();
+        int i = ctx.Customers.Count();
+        lblMessage.Text = string.Format("Found {0} customers.", i);
+    }
+
+    protected void ShowBooks()
+    {
+        gvBooks.DataSource = ctx.Books.ToList();
+        gvBooks.DataBind();
+        int i = ctx.Books.Count();
+        lblMessage.Text = string.Format("Found {0} books.", i);
+    }
+
+    protected void FillControls()
+    {
+        //var returnValue = ctx.Customers;
+        //var returnValue = from c in ctx.Customers orderby c.lastname select c;
+        var returnValue = ctx.Customers.OrderBy(c => c.lastname);
+        ddlCustomers.DataSource = returnValue.ToList();
+        ddlCustomers.DataTextField = "lastname";
+        ddlCustomers.DataValueField = "id";
+        ddlCustomers.DataBind();
+        // add empty item in front
+        ddlCustomers.Items.Insert(0, string.Empty);
+    }
+
+    protected void ShowCustomersOrders(Customer c)
+    {
+        ltResult.Text = string.Format("Customer {0} {1} has {2} order(s).", c.firstname ,c.lastname, c.Orders.Count);
+
+        foreach (var o in c.Orders)
+        {
+            ltResult.Text += string.Format("<br>- order {0} holding {1} book(s):", o.odate.ToShortDateString(), o.Orderitems.Count);
+            foreach (var i in o.Orderitems)
+            {
+                ltResult.Text += string.Format("<br>-- book {0} by {1}, {2} item(s)", i.Book.name, i.Book.author, i.count); 
+            }
+        }
+    }
+    #endregion
+
+    protected void btnGetCustomers_Click(object sender, EventArgs e)
+    {
+        ShowCustomers();
+    }
+
+    protected void btnGetBooks_Click(object sender, EventArgs e)
+    {
+        ShowBooks();
+    }
+
+    protected void ddlCustomers_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (ddlCustomers.SelectedIndex > 0)
+        {
+            int id = int.Parse(ddlCustomers.SelectedValue);
+            //var returnValue = from c in ctx.Customers where c.id == id select c;
+            var returnValue = ctx.Customers.Where(c => c.id == id);
+            Customer customer = returnValue.FirstOrDefault();
+            ShowCustomersOrders(customer); 
+        }
+    }
+}
