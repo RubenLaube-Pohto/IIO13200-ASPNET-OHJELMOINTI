97ae9918528c6c561894a393b9dbeeb23bb9a6c6
Ruben Laube-Pohto <h8871@student.jamk.fi>
2016-11-10 17:17:13 +0200

    Commit lesson

--- Saitti/BookShop.aspx
+++ Saitti/BookShop.aspx
@@ -1,24 +1,35 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="BookShop.aspx.cs" Inherits="BookShop" %>
 
-<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
     <style>
         img {
             width: 100%;
         }
     </style>
 </asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="body" Runat="Server">
+<asp:Content ID="Content2" ContentPlaceHolderID="body" runat="Server">
     <h1>My Book Shop</h1>
     <div class="w3-row">
         <!-- customers -->
         <div class="w3-container w3-third">
             <asp:Button ID="btnGetCustomers" CssClass="w3-btn w3-blue" runat="server" Text="Get customers" OnClick="btnGetCustomers_Click" />
-            <asp:DropDownList ID="ddlCustomers" runat="server" OnSelectedIndexChanged="ddlCustomers_SelectedIndexChanged" AutoPostBack="true"></asp:DropDownList>
             <asp:GridView ID="gvCustomers" CssClass="w3-table" runat="server"></asp:GridView>
         </div>
+        <!-- customer crud -->
+        <div class="w3-container w3-third">
+            <h2>CRUD operations</h2>
+            <asp:DropDownList ID="ddlCustomers" runat="server" OnSelectedIndexChanged="ddlCustomers_SelectedIndexChanged" AutoPostBack="true"></asp:DropDownList><br />
+            firstname:
+            <asp:TextBox ID="txtFirstName" runat="server"></asp:TextBox><br />
+            lastname:
+            <asp:TextBox ID="txtLastName" runat="server"></asp:TextBox><br />
+            <asp:Button ID="btnNew" CssClass="w3-btn" runat="server" Text="New" OnClick="btnNew_Click"/>
+            <asp:Button ID="btnSave" CssClass="w3-btn" runat="server" Text="Save" OnClick="btnSave_Click"/>
+            <asp:Button ID="btnDelete" CssClass="w3-btn" runat="server" Text="Delete" />
+        </div>
         <!-- books -->
-        <div class="w3-container w3-twothird">
-            <asp:Button ID="btnGetBooks" CssClass="w3-btn w3-blue" runat="server" Text="Get books" OnClick="btnGetBooks_Click"/>
+        <div class="w3-container w3-third">
+            <asp:Button ID="btnGetBooks" CssClass="w3-btn w3-blue" runat="server" Text="Get books" OnClick="btnGetBooks_Click" />
             <asp:GridView ID="gvBooks" CssClass="w3-table" runat="server"></asp:GridView>
         </div>
     </div>
@@ -26,7 +37,6 @@
         <asp:Literal ID="ltResult" runat="server"></asp:Literal>
     </div>
 </asp:Content>
-<asp:Content ID="Content3" ContentPlaceHolderID="footer" Runat="Server">
+<asp:Content ID="Content3" ContentPlaceHolderID="footer" runat="Server">
     <asp:Label ID="lblMessage" runat="server" Text=""></asp:Label>
 </asp:Content>
-
--- Saitti/BookShop.aspx.cs
+++ Saitti/BookShop.aspx.cs
@@ -8,6 +8,7 @@ using System.Web.UI.WebControls;
 public partial class BookShop : System.Web.UI.Page
 {
     protected static BookShopEntities ctx;
+    protected static bool isCustomerSelected;
 
     protected void Page_Load(object sender, EventArgs e)
     {
@@ -46,18 +47,30 @@ public partial class BookShop : System.Web.UI.Page
         ddlCustomers.DataBind();
         // add empty item in front
         ddlCustomers.Items.Insert(0, string.Empty);
+        // for crud
+        txtFirstName.Text = string.Empty;
+        txtLastName.Text = string.Empty;
+        SetButtons();
+    }
+
+    protected void SetButtons()
+    {
+        // control usability of buttons
+        btnNew.Enabled = !isCustomerSelected;
+        btnSave.Enabled = isCustomerSelected;
+        btnDelete.Enabled = isCustomerSelected;
     }
 
     protected void ShowCustomersOrders(Customer c)
     {
-        ltResult.Text = string.Format("Customer {0} {1} has {2} order(s).", c.firstname ,c.lastname, c.Orders.Count);
+        ltResult.Text = string.Format("Customer {0} {1} has {2} order(s).", c.firstname, c.lastname, c.Orders.Count);
 
         foreach (var o in c.Orders)
         {
             ltResult.Text += string.Format("<br>- order {0} holding {1} book(s):", o.odate.ToShortDateString(), o.Orderitems.Count);
             foreach (var i in o.Orderitems)
             {
-                ltResult.Text += string.Format("<br>-- book {0} by {1}, {2} item(s)", i.Book.name, i.Book.author, i.count); 
+                ltResult.Text += string.Format("<br>-- book {0} by {1}, {2} item(s)", i.Book.name, i.Book.author, i.count);
             }
         }
     }
@@ -81,7 +94,62 @@ public partial class BookShop : System.Web.UI.Page
             //var returnValue = from c in ctx.Customers where c.id == id select c;
             var returnValue = ctx.Customers.Where(c => c.id == id);
             Customer customer = returnValue.FirstOrDefault();
-            ShowCustomersOrders(customer); 
+            ShowCustomersOrders(customer);
+            isCustomerSelected = true;
+            txtFirstName.Text = customer.firstname;
+            txtLastName.Text = customer.lastname;
+            SetButtons();
+        }
+        else
+        {
+            ltResult.Text = string.Empty;
+            isCustomerSelected = false;
+            txtFirstName.Text = string.Empty;
+            txtLastName.Text = string.Empty;
+            SetButtons();
+        }
+    }
+
+    protected void btnNew_Click(object sender, EventArgs e)
+    {
+        // create a new customer to context if one with the same name doesn't exist
+        bool isThere = ctx.Customers.Any(c => c.firstname.Contains(txtFirstName.Text) && c.lastname.Contains(txtLastName.Text));
+        if (isThere)
+        {
+            lblMessage.Text = string.Format("Customer {0} {1} already exists.", txtFirstName.Text, txtLastName.Text);
+        }
+        else
+        {
+            Customer customer = new Customer();
+            customer.firstname = txtFirstName.Text;
+            customer.lastname = txtLastName.Text;
+            ctx.Customers.Add(customer);
+            ctx.SaveChanges();
+            lblMessage.Text = string.Format("New customer {0}: {1} {2} created successfully",
+                customer.id,
+                customer.firstname,
+                customer.lastname);
+            FillControls();
+        }
+    }
+
+    protected void btnSave_Click(object sender, EventArgs e)
+    {
+        // get selected customer id
+        int id = int.Parse(ddlCustomers.SelectedValue);
+        if (id > 0)
+        {
+            var ret = ctx.Customers.Where(c => c.id == id);
+            Customer customer = ret.FirstOrDefault();
+            if (customer != null)
+            {
+                if (customer.firstname != txtFirstName.Text)
+                    customer.firstname = txtFirstName.Text;
+                if (customer.lastname != txtLastName.Text)
+                    customer.lastname = txtLastName.Text;
+                ctx.SaveChanges();
+                FillControls();
+            }
         }
     }
 }
