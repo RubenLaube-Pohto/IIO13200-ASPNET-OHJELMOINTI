25772ef677e1ec3cba27be4bc8aa35a25420eeab
Ruben Laube-Pohto <h8871@student.jamk.fi>
2016-11-17 16:18:12 +0200

    Add pre existing Turva0 site

---
+++ Turva0/ValidateRequest.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class ValidateRequest : System.Web.UI.Page
+{
+  //http://stackoverflow.com/questions/502112/handling-request-validation-silently
+  //TODO ylikirjoita ProcessRequest
+  protected void Page_Load(object sender, EventArgs e)
+  {
+    try
+    {
+
+    }
+    catch (Exception ex)
+    {
+      ltMessage.Text = ex.Message;  
+    }
+  }
+
+  protected void btnCommit_Click(object sender, EventArgs e)
+  {
+    try
+    {
+      //TODO kirjoitetaan kommentti XML:ään
+    }
+    catch (Exception ex)
+    {
+      ltMessage.Text = ex.Message;
+    }
+  }
+}
